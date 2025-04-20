d165f19fba44f1e55fd43786f104a1801550a599
Foram0105 <solankiforam87@gmail.com>
2025-04-20 23:46:13 +0530

    Update main2.py
    main file

--- main2.py
+++ main2.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-# import plotly.graph_objects as go
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
